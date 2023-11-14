vivavy/pyhoney

commit a92510c57c1469caaade29ea3424bba58b8d8646

Add `*.hny` format support for micro editor

Makefile
@@ -13,6 +13,7 @@ install:
 		  ~/.config/sublime-text/Packages
 	@cp *.py /usr/share/$(NAME)
 	@cp support/sublime/Honey.sublime-syntax ~/.config/sublime-text/Packages/Honey.sublime-syntax
+	@cp support/sublime/hny.yaml ~/.config/micro/syntax/
 	@rm /usr/share/$(NAME)/update.py
 	@cp grammar/*.glr /usr/share/$(NAME)/grammar
 	@cp lds/*.ld /usr/share/$(NAME)/lds

.gitignore
@@ -168,3 +168,4 @@ cython_debug/
 *.pgt
 *.pyc
 *__pycache__*
+*.hl

example.hny
@@ -2,7 +2,6 @@
 
 format multiboot
 
-
 # videobuffer #
 video: u8[] = 0xb8000 as u8[]
 
@@ -18,6 +17,3 @@ fn main(argc: int, argv: str[]) -> int {
 	}
 	leave # bootloader hangs automatically after kernel return #
 }
-
-
-

main.py
@@ -12,7 +12,7 @@ import traceback
 # vivavy: formats
 
 # format detection is very simple
-def detect_format(i):
+def detect_format(i: str):
     with open(i, "rt") as f:
         hnyir.parse(f.read())
     return hnyir.data["format"]

support/micro/hny.yaml
@@ -1,0 +1,43 @@
+filetype: hny
+
+detect:
+    filename: "\\.hny$"
+
+rules:
+      # Reserved words
+    - statement: "\\b(format|fn|forever|leave|as)\\b"
+      # Constants
+    - green: "\\b[A-Z][A-Z_0-9]+\\b"
+      # Numbers
+    - constant.number: "\\b[0-9]+\\b"
+    - constant.number: "\\b0[Xx]([0-9a-fA-F]+)\\b"
+    - constant.number: "\\b0[Oo]([0-7]+)\\b"
+    - constant.number: "\\b0[Bb]([01]+)\\b"
+      # Booleans
+    - constant: "\\b(true|false)\\b"
+    - type: "\\b[a-zA-Z_0-9]+\\_t\\b"
+      # Builtin types that start with lowercase.
+    - type: "\\b(bool|string|isize|usize|((i|u)(8|16|32|64))|float|int|str)\\b"
+
+    - lightgreen: "\\-\\>"
+
+    - constant.string:
+        start: "\""
+        end: "\""
+        skip: '\\.'
+        rules:
+            - constant.specialChar: '\\.'
+
+    - comment:
+        start: "#"
+        end: "#"
+        rules:
+            - todo: "(TODO|XXX|FIXME):?"
+
+    - constant.string:
+            start: "'"
+            end: "'"
+            skip: "\\\\."
+            rules:
+                - constant.specialChar: "\\\\([\"'abfnrtv\\\\]|[0-3]?[0-7]{1,2}|x[0-9A-Fa-f]{1,2}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})"
+    

commit ef6b30f43f7471c597fa2c7a5d95428ea0a3e562

Fix Makefile
Broken by https://github.com/vivavy/pyhoney/commit/a92510c57c1469caaade29ea3424bba58b8d8646

Makefile
@@ -8,14 +8,14 @@ main:
 	@exit
 
 install:
-	@mkdir -p /usr/share/$(NAME) /usr/share/$(NAME)/grammar \
-		  /usr/share/$(NAME)/objects /usr/share/$(NAME)/lds \
-		  ~/.config/sublime-text/Packages
+	@mkdir -p /usr/share/$(NAME) /usr/share/$(NAME)/objects \
+		/usr/share/$(NAME)/lds ~/.config/sublime-text/Packages \
+		~/.config/micro/syntax/
 	@cp *.py /usr/share/$(NAME)
 	@cp support/sublime/Honey.sublime-syntax ~/.config/sublime-text/Packages/Honey.sublime-syntax
-	@cp support/sublime/hny.yaml ~/.config/micro/syntax/
+	@cp support/micro/hny.yaml ~/.config/micro/syntax/
 	@rm /usr/share/$(NAME)/update.py
-	@cp grammar/*.glr /usr/share/$(NAME)/grammar
+	@cp grammar.antlr /usr/share/$(NAME)/
 	@cp lds/*.ld /usr/share/$(NAME)/lds
 	@echo
 